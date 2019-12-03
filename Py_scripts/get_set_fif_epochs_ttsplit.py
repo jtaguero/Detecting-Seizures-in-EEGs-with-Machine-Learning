8ac513b8759e252d8a9cc969436198fb263f4835
John Aguero <johnaguero@michellsmacbook.attlocal.net>
2019-12-03 11:32:30 -0600

    annotated ttsplit

--- Py_scripts/get_set_fif_epochs_ttsplit.py
+++ Py_scripts/get_set_fif_epochs_ttsplit.py
@@ -3,7 +3,7 @@ def get_set_fif_epochs_ttsplit(data_path = 'fif/',  fif_list=fif_lst ):
         
 
         data_path = 'fif/'
-
+        #get all files in fif list and concatenate them into one raw object
         raws = mne.concatenate_raws([mne.io.read_raw_fif('fif/' + f, preload=False) for f in fif_lst])
         info = raws.info
         channels = raws.ch_names
@@ -11,7 +11,7 @@ def get_set_fif_epochs_ttsplit(data_path = 'fif/',  fif_list=fif_lst ):
 
         event_id = dict(Seizure=1, Nonseizure=0)
         events_from_annot, event_dict = mne.events_from_annotations(raws, event_id=event_id, chunk_duration=1)
-
+        #get all events from the annotations previously set in the fif files and divide data into one second epochs labeled Seizure=1 or Nonseizure=0
         tmin, tmax = -0.5, 0.5
         epochs = mne.Epochs(raws, events_from_annot, event_id, tmin, tmax, proj=True,
                             decim=2, baseline=None, preload=True)
@@ -21,12 +21,17 @@ def get_set_fif_epochs_ttsplit(data_path = 'fif/',  fif_list=fif_lst ):
         # get MEG and EEG data
         
         picks = mne.pick_types(epochs.info, meg=False, eeg=True)
+
+        #separate data and target
         X = epochs.get_data()[:, picks]
         y = epochs.events[:, 2]
-        X.shape, y.shape
-
+        
+        #reshape X for test train split
         X_2d = X.reshape(len(X), -1)
 
+        #test train split with y stratified due to the our unbalanced classification problem,
+        #so that the proportion of seizure and non-seizure categories are the same in the training and testing sets
+        
         from sklearn.model_selection import train_test_split
         X_train, X_test, y_train, y_test = train_test_split(
         X_2d, y, stratify=y)
